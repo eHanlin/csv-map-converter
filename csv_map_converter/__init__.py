77fc8b13879f66e803899d9af6b092a7327f70c7
Sparrow.jang <sparrow.jang@gmail.com>
2017-02-21 18:58:01 +0800

    removed unneeded

--- csv_map_converter/__init__.py
+++ csv_map_converter/__init__.py
@@ -1,13 +1,13 @@
 
-import csv
+#import csv
 from .Converter import Converter
 
 converter = Converter()
 
 def convert(lines, start_row = 1, title_row = 0):
 
-    if isinstance(type(lines), str):
-        csv.reader(lines)
+    #if isinstance(type(lines), str):
+    #    lines = [for line in csv.reader(lines)]
 
     return converter.convert(lines, start_row, title_row)
 
