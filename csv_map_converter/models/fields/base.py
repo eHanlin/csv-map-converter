870cf8fd997b57d3d3e1257f66155d2f4d583c54
Sparrow.jang <sparrow.jang@gmail.com>
2017-02-22 15:01:46 +0800

    added base fields

---
+++ csv_map_converter/models/fields/base.py
@@ -0,0 +1,50 @@
+
+class Field():
+
+    def to_python(self, value):
+        return value
+
+class ListField(Field):
+
+    def __init__(self, subfield = Field()):
+        self.__subfield = subfield
+
+    def to_python(self, values):
+        if values:
+            return [subfield.to_python(value) for value in values]
+        return None
+
+class BooleanField(Field):
+
+    def to_python(self, value):
+        if value:
+            value = value.lower()
+            if not(value in ["0", "false"]): return True
+
+        return False
+            
+
+class StringField(Field): pass
+
+class LongField(Field):
+
+    def to_python(self, value):
+        if value:
+            return long(value)
+        return None
+
+class IntField(Field):
+
+    def to_python(self, value):
+        if value:
+            return int(value)
+        return None
+
+class FloatField(Field):
+
+    def to_python(self, value):
+        if value:
+            return float(value)
+        return None
+
+
