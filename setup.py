ffd85f96bda0e29afd62a4ae84c18b146c0e5b66
Sparrow.jang <sparrow.jang@gmail.com>
2017-02-22 12:19:10 +0800

    added url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='csv-map-converter',
       description='It is csv converter to be able to get multi-values.',
       author='Sparrow Jang',
       author_email='sparrow.jang@gmail.com',
-      url='',
+      url='https://github.com/eHanlin/csv-map-converter',
       packages=find_packages(),
       test_suite = 'tests.test_converter',
      )
